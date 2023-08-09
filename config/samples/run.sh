47794f349f3b058c129090ab4c9415ee71a5987b
michael levan <michael.levan@orbica.world>
2023-08-09 15:41:40 -0400

    LB info

--- config/samples/run.sh
+++ config/samples/run.sh
@@ -2,6 +2,7 @@ export POOL_SIZE=
 export CLUSTER_NAME=
 export PUB_SSH_KEY=
 export VM_SIZE=
+export CONTROL_PLANE_LB=
 
 clusterctl generate cluster rackncluster \
   --kubernetes-version v1.27.3 \
