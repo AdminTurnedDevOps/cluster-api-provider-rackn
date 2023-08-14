d546ca9b5745fc46b6b6e00cf74c1279b4be8af8
michael levan <michael.levan@orbica.world>
2023-08-14 07:56:14 -0400

    Reconciliation count

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -83,7 +84,9 @@ func main() {
 	if err = (&controllers.RackNk8sclusterReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(ctx, mgr); err != nil {
+	}).SetupWithManager(ctx, mgr, controller.Options{
+		MaxConcurrentReconciles: 10,
+	}); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "RackNk8scluster")
 		os.Exit(1)
 	}
